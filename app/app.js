b690b897297dd459aa7e0f425e146fb712aaa46f
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-04-07 02:59:58 -0400

    clean up code

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
 const customerRouter = require('../api/routes/customers')
